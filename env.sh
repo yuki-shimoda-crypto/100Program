f1a9abd1f2c9814c38e7ee237fbe94879d05f31c
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-09-13 12:37:17 +0900

    add env.sh

--- env.sh
+++ env.sh
@@ -6,7 +6,7 @@ apt update
 apt upgrade -y 
 
 # install python
-apt install python3.9 python3-pip python3.9-venv vim ssh git -y 
+apt install curl python3.9 python3-pip python3.9-venv vim ssh git snapd -y
 
 # Python3.8 -> Python3.9
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 9 
@@ -15,3 +15,7 @@ update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 8
 # python module
 python3 -m pip install Django
 python3 -m pip install janome
+python3 -m pip install django-heroku
+
+# install heroku
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
